c3bd2c68d22cd40719aba96adcf92b764e3d9b2b
Lucas Churchill <lucas@agtlucas.com>
2014-08-16 20:49:26 -0300

    Update index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var pg = require('pg');
 
 app.use(logfmt.requestLogger());
 
@@ -11,4 +12,12 @@ app.get('/', function(req, res) {
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  client.query('SELECT * FROM your_table', function(err, result) {
+    done();
+    if(err) return console.error(err);
+    console.log(result.rows);
+  });
 });
