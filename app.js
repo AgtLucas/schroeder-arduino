c120d65f6575d072bc57030cc66d0a29fa1a3282
Fabrício Ronchi <fabricioronchii@gmail.com>
2014-10-07 10:31:32 -0300

    Testes

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express        = require('express')
-  , app            = express()
-  , cors           = require('cors')
+  , app            = express();
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+  });
+
+var cors           = require('cors')
   , bodyParser     = require('body-parser')
   , errorHandler   = require('errorhandler')
   , methodOverride = require('method-override')
