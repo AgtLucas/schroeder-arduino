6c47e77d2a49270f23b11441e4187211901ba00c
Fabrício Ronchi <fabricioronchii@gmail.com>
2014-10-07 10:28:31 -0300

    Testes

--- app.js
+++ app.js
@@ -152,17 +152,7 @@ db.sequelize.sync().complete(function(err) {
   } else {
     http.listen(app.get('port'), function(){
       console.log('Express server listening on port ' + app.get('port'))
-      io = require('socket.io')(http);
-    io.set('origins','*');
-    io.set('transports', [
-      'websocket',
-      'flashsocket',
-      'htmlfile',
-      'xhr-polling',
-      'jsonp-polling'
-    ]);
+      io = require('socket.io')(http, { origins:'fabricioronchi.com:* http://fabricioronchi.com:* http://www.fabricioronchi.com:* fabricioronchi.com/arduino:* http://fabricioronchi.com/arduino:* http://www.fabricioronchi.com/arduino:*'});
     });
-
-
   }
 })
