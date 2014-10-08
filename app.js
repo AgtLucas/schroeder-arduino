fe2e52f837ad7c00bf3faa4a9663db2735b711fa
Fabrício Ronchi <fabricioronchii@gmail.com>
2014-10-08 10:25:37 -0300

    Socket

--- app.js
+++ app.js
@@ -162,11 +162,16 @@ app.post('/schroeder/users', users.newUser)
 app.put('/schroeder/users/:id', users.update)
 app.del('/schroeder/users/:id', users.destroy)
 
+var io;
 
-
-http.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'))
-});
-
-var io = require('socket.io')(http);
+db.sequelize.sync().complete(function(err) {
+  if (err) {
+    throw err
+  } else {
+    http.listen(app.get('port'), function(){
+      console.log('Express server listening on port ' + app.get('port'))
+    });
+    io = require('socket.io')(http);
+  }
+})
 
