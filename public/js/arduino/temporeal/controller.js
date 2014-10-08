155a957fb764986ece2899dda637e86df70e1e25
Fabrício Ronchi <fabricioronchii@gmail.com>
2014-10-08 11:05:47 -0300

    fabricio

--- public/js/arduino/temporeal/controller.js
+++ public/js/arduino/temporeal/controller.js
@@ -1,10 +1,27 @@
 define(['js/app', 'socketIO'], function (app, socketIO) {
 		app.controller('tempoReal', function ($scope, $http) {
+
   	var socket = socketIO();
 
-  	socket.on('new-medicao', function(obj){
-    	alert();
+  	angular.extend($scope, {
+  		data: "00:00 00/00/0000",
+  		humidade: "0",
+  		temperatura: "0"
   	});
+
+  	socket.on('new-medicao', function(obj){
+    	angular.extend($scope, {
+  			data: $scope.getData(obj.createdAt),
+  			humidade: obj.humidity,
+  			temperatura: obj.temperature
+  		});
+  	});
+
+  	$scope.getData = function(data){
+  		var _data = data.split("T")[0].split("-")[2] + "/" + data.split("T")[0].split("-")[1] + "/" + data.split("T")[0].split("-")[0];
+  		var _hora = teste.split("T")[1].split(":")[0] + ":" + teste.split("T")[1].split(":")[1];
+  		return _hora + " " + _data;
+  	};
 
 	  });
 
