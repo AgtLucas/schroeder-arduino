a71f19a8bd9af100f87e6b8351f23b31a13a32fe
Fabrício Ronchi <fabricioronchii@gmail.com>
2014-10-15 15:25:07 -0300

    Bootstrap

--- public/arduino/js/app.js
+++ public/arduino/js/app.js
@@ -2,10 +2,10 @@ function iniciar(){
   var estado = document.getElementById("estado").innerHTML;
   if(estado === "0"){
       document.getElementById("estado").innerHTML="1";
-      document.getElementById("desligarrele").style.display = "";
+      document.getElementById("desligarbuzzer").style.display = "";
   } else {
       document.getElementById("estado").innerHTML="0";
-      document.getElementById("ligarrele").style.display = "";
+      document.getElementById("ligarbuzzer").style.display = "";
   }
 
   /*var xmlhttp;
@@ -35,5 +35,6 @@ function iniciar(){
 }
 
 function acao(acao, descricao){
+  alert(descricao);
   window.location.href = acao;
 };
