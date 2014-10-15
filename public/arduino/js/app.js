13c5f5543497d7f268d77bf4c4359c291748c72f
Fabrício Ronchi <fabricioronchii@gmail.com>
2014-10-15 13:52:24 -0300

    Bootstrap

--- public/arduino/js/app.js
+++ public/arduino/js/app.js
@@ -7,7 +7,24 @@ function iniciar(){
       document.getElementById("estado").innerHTML="0";
       document.getElementById("ligarrele").style.display = "";
   }
-};
+
+  var xmlhttp;
+  if (window.XMLHttpRequest) {
+    xmlhttp=new XMLHttpRequest();
+  } else {
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
+  xmlhttp.onreadystatechange = function() {
+    //if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+      console.log(xmlhttp.responseText);
+    //}
+  }
+
+  xmlhttp.open("GET", "http://schroeder-arduino.herokuapp.com/schroeder/autenticar/S?acao=Entrou%20no%20escrit%C3%B3rio", true);
+  xmlhttp.send();
+
+}
 
 function acao(acaoExecutar){
   alert(acaoExecutar);
