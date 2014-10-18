7e8ae734ff9800554b432a9b126d6b0c961683c8
Fabrício Ronchi <fabricioronchii@gmail.com>
2014-10-18 16:08:40 -0300

    Final

--- routes/log.js
+++ routes/log.js
@@ -1,7 +1,7 @@
 var db = require('../models')
 
 exports.findAll = function(req, res, next) {
-  db.Log.findAll({ order: 'createdAt DESC', include: [ db.User ], limit: 100 }).success(function(entities) {
+  db.Log.findAll({ include: [ db.User ] }).success(function(entities) {
     res.json(entities)
   })
 }
